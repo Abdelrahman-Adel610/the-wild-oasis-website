2bdcd8125a5878cc89708e15a8d10f6264bf3b7b
Abdelrahman Adel <engba80818233@gmail.com>
2025-07-09 01:10:24 +0300

    feat: configured font basic metadata for the page

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,17 @@
 import "./globals.css";
-
+import { Josefin_Sans } from "next/font/google";
+export const metadata = {
+  title: {
+    template: "The wild oasis | %s",
+    default: "The wild oasis | Home",
+  },
+  description: "website for booking awesome cabins",
+};
+const font = Josefin_Sans({ display: "swap", subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body className={`${font.className}`}>{children}</body>
     </html>
   );
 }
