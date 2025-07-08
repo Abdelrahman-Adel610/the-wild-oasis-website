5d85ab53e43146b8a4b7580b60ec36cdb0a296a6
Abdelrahman Adel <engba80818233@gmail.com>
2025-07-09 01:28:47 +0300

    feat: added Home page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
+import Navigation from "@/app/_components/Navigation";
 import "./globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Logo from "@/app/_components/Logo";
+import Header from "@/app/_components/Header";
 export const metadata = {
   title: {
     template: "The wild oasis | %s",
@@ -11,7 +14,16 @@ const font = Josefin_Sans({ display: "swap", subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${font.className}`}>{children}</body>
+      <body
+        className={`${font.className}  antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <header>
+          <Header />
+        </header>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
